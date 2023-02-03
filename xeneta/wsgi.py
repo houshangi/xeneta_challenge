8f5caeddd6e2d4c8ff77c9d282d6da86cdd72fcd
morteza <mhoushangi1998@gmail.com>
2023-02-03 20:47:15 +0330

    wsgi.py : reformat the code with black

--- xeneta/wsgi.py
+++ xeneta/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xeneta.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xeneta.settings")
 
 application = get_wsgi_application()
