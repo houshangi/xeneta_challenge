99c203aaa2ad70738c32860bd262c95575e9803e
morteza <mhoushangi1998@gmail.com>
2023-02-03 20:49:14 +0330

    query_handler.py : Class For query validation and execution

---
+++ rates/queries/query_handler.py
@@ -0,0 +1,96 @@
+import re
+from django.db import connection
+from rest_framework import exceptions
+
+
+class QueryHandler:
+    """
+    The QueryHandler class contains methods for validating parameters and executing
+    a query to retrieve average prices
+    for a specific origin and destination port within a date range.
+
+    The class uses regular expression to validate date input and Django's
+    connection to execute the query.
+    In case of invalid input,
+     it raises ValidationError from the rest_framework exceptions.
+
+    Attributes:
+        date_regex (re.Pattern): A regular expression pattern to match the date format.
+    """
+
+    date_regex = re.compile(r"^\d{4}-\d{2}-\d{2}$")
+
+    @staticmethod
+    def validate_params(origin, destination, date_from, date_to):
+        """
+        Validates the input parameters for origin, destination, date_from and date_to.
+
+        Args:
+            origin (str): The code of the origin port.
+            destination (str): The code of the destination port.
+            date_from (str): The start date in the format 'YYYY-MM-DD'.
+            date_to (str): The end date in the format 'YYYY-MM-DD'.
+
+        Raises:
+            ValidationError: If the origin is None or not a string.
+                             If the destination is None or not a string.
+                             If the date_from does not match the format 'YYYY-MM-DD'.
+                             If the date_to does not match the format 'YYYY-MM-DD'.
+        """
+        if origin is None or not isinstance(origin, str):
+            raise exceptions.ValidationError(
+                {"origin": "origin must be a string and cannot be None"}
+            )
+
+        if destination is None or not isinstance(destination, str):
+            raise exceptions.ValidationError(
+                {"destination": "destination must be a string and cannot be None"}
+            )
+
+        if not QueryHandler.date_regex.match(date_from):
+            raise exceptions.ValidationError(
+                {"date_from": "date_from must be in the format 'YYYY-MM-DD'"}
+            )
+
+        if not QueryHandler.date_regex.match(date_to):
+            raise exceptions.ValidationError(
+                {"date_to": "date_to must be in the format 'YYYY-MM-DD'"}
+            )
+
+    @staticmethod
+    def execute_query(origin, destination, date_from, date_to):
+        """
+        Executes a query to retrieve average prices for a specific
+        origin and destination port within a date range.
+
+        Args:
+            origin (str): The code of the origin port.
+            destination (str): The code of the destination port.
+            date_from (str): The start date in the format 'YYYY-MM-DD'.
+            date_to (str): The end date in the format 'YYYY-MM-DD'.
+
+        Returns:
+            result (List[Tuple[str, float]]): A list of tuples containing the date and average price.
+        """
+        query = f"""
+            SELECT day AS day,
+            CASE  WHEN COUNT(*) >= 3 then
+            AVG(price) ELSE NULL
+            END 
+            AS avg_price
+            FROM prices
+            LEFT JOIN ports AS origin_port ON origin_port.code = prices.orig_code
+            LEFT JOIN ports AS dest_port ON dest_port.code = prices.dest_code
+            WHERE (origin_port.code = %s OR origin_port.parent_slug = %s)
+            AND (dest_port.code = %s OR dest_port.parent_slug  = %s)
+            AND day BETWEEN %s AND %s
+            GROUP BY day
+        """
+
+        with connection.cursor() as cursor:
+            cursor.execute(
+                query, [origin, origin, destination, destination, date_from, date_to]
+            )
+            result = cursor.fetchall()
+
+        return result
