c0e80274d27a2def011675bf8f083d5f33972d72
morteza <mhoushangi1998@gmail.com>
2023-02-04 15:54:08 +0330

    test_api.py: add api unit testing

--- rates/tests/test_api.py
+++ rates/tests/test_api.py
@@ -1,9 +1,46 @@
-from django.test import TestCase
-from django.db import connection
+from unittest import TestCase
+from rest_framework import exceptions
+from rest_framework.test import APIRequestFactory
+from ..views import AveragePriceList
+from django.test import Client
 
 
-# Create your tests here.
-class APITestCase(TestCase):
-    def test_one(self):
-        with connection.cursor() as cursor:
-            cursor.execute("select * from prices")
+class AveragePriceListTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_valid_params(self):
+        # Test case for valid parameters
+        response = self.client.get(
+            "/api/rates?origin=CNGGZ&destination=NOOSL&date_from=2016-01-01&date_to=2016-01-28"
+        )
+        print(response.content)
+        self.assertEqual(response.status_code, 200)
+
+    def test_none_origin_param(self):
+        # Test case for none origin parameter
+        response = self.client.get(
+            "/api/rates?destination=NOOSL&date_from=2016-01-01&date_to=2016-01-28"
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_none_destination_param(self):
+        # Test case for none destination parameter
+        response = self.client.get(
+            "/api/rates?origin=CNSGH&date_from=2016-01-01&date_to=2016-01-28"
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_invalid_date_from_param(self):
+        # Test case for invalid date_from parameter
+        response = self.client.get(
+            "/api/rates?origin=CNSGH&destination=NOOSL&date_from=2022-13-01&date_to=2022-01-10"
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_invalid_date_to_param(self):
+        # Test case for invalid date_to parameter
+        response = self.client.get(
+            "/api/rates?origin=AMS&destination=MOW&date_from=2016-01-01&date_to=2016-13-10"
+        )
+        self.assertEqual(response.status_code, 400)
