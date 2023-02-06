3904e999c140f4d5644be4c19e774c9191acdefb
morteza <mhoushangi1998@gmail.com>
2023-02-06 22:47:13 +0330

    add empty list Response Unit test

--- rates/tests/test_api.py
+++ rates/tests/test_api.py
@@ -83,3 +83,17 @@ class AveragePriceListTestCase(TestCase):
             b'{"date_from"' b':"date_from cannot' b' be after date_to"}',
         )
 
+    def test_empty_list_in_response(self):
+        #test to generate empty list with valid but not meaningful names
+        response = self.client.get(
+            "/api/rates"
+            "?date_from=2016-01-01"
+            "&date_to=2016-01-03"
+            "&origin=notmeaningfulorigin"
+            "&destination=notmeaningfuldestination"
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.content,
+            b"[]",
+        )
